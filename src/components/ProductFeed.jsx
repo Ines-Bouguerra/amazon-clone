5f8e864cb163d8b0b6c74175997028ba95a5299a
Ines Bouguerra <i.bouguerra@numeryx.fr>
2023-05-31 11:54:14 +0100

    Fix products disposition

--- src/components/ProductFeed.jsx
+++ src/components/ProductFeed.jsx
@@ -3,8 +3,31 @@ import Product from './Product'
 
 const ProductFeed = ({ products }) => {
     return (
-        <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-            {products.products.map(({ id, title, price, description, category, image }) => (
+        <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
+            {products.products.slice(0, 4).map(({ id, title, price, description, category, image }) => (
+                <Product
+                    key={id}
+                    id={id}
+                    title={title}
+                    price={price}
+                    description={description}
+                    category={category}
+                    image={image} />
+            ))}
+            <img className='md:col-span-full' src="https://links.papareact.com/dyz" alt="ad" />
+            <div className='md:col-span-2'>
+                {products.products.slice(4, 5).map(({ id, title, price, description, category, image }) => (
+                    <Product
+                        key={id}
+                        id={id}
+                        title={title}
+                        price={price}
+                        description={description}
+                        category={category}
+                        image={image} />
+                ))}
+            </div>
+            {products.products.slice(5, products.products.length).map(({ id, title, price, description, category, image }) => (
                 <Product
                     key={id}
                     id={id}
